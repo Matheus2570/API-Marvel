48eb549f5e7ea3f759ffa663c29a3f1dd3bb6e2c
Matheus de Carvalho <matheusdecarvalho100@gmail.com>
2025-06-18 11:24:22 -0300

    Dia 18/06/2025

---
+++ src/pages/DetalhesHerois.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DetalhesHerois.css';
+
+function Detalhes() {
+  return (
+    <div  style={{ textAlign: 'center' }}>
+      <h1>Detalhes do Herói</h1>
+    </div>
+  );
+}
+
+export default Detalhes;
